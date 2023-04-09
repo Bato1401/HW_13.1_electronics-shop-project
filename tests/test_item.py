319a8a61ab9c2d4c1fad4e992456edf35bb4c680
Bato1401 <lego.1401@gmail.com>
2023-04-09 17:54:25 +0300

    Прописали тесы для класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Ноутбук", 5000, 2)
+
+
+def test_calculate_total_price(item):
+    """Проверка общей стоимости имеющихся товаров"""
+    assert item.calculate_total_price() == 10000
+
+
+def test_apply_discount(item):
+    """Проверка правильности установки скидки на товар"""
+    assert item.apply_discount() == 5000
