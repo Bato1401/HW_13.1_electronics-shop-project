c5d0cdbbc6beb9b70bbbf547da7b12f48a6b8fe1
Bato1401 <lego.1401@gmail.com>
2023-04-23 19:42:00 +0300

    add test_add

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -8,10 +8,20 @@ def phone():
     return Phone("iPhone 14", 120_000, 5, 2)
 
 
+@pytest.fixture
+def other():
+    return Phone('Телевизор', 20000, 5, 2)
+
+
 def test__repr_(phone):
     """Тест для repr"""
     assert repr(phone) == "Phone('iPhone 14', 120000, 5, 2)"
 
+
 def test__str_(phone):
     """Тест для str"""
     assert str(phone) == 'iPhone 14'
+
+
+def test_add(phone, other):
+    assert other + phone == 10
