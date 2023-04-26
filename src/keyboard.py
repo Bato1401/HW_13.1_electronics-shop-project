6cd100b511ebd2c00161e92281af7d6349c12898
Bato1401 <lego.1401@gmail.com>
2023-04-26 20:30:45 +0300

    add new class KeyBoard and new class MixinChL

---
+++ src/keyboard.py
@@ -0,0 +1,26 @@
+from src.item import Item
+
+
+class MixinChL:
+    """Класс миксин"""
+    def __init__(self):
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
+        return self
+
+
+class KeyBoard(Item, MixinChL):
+    """Класс для клавиатур"""
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+
